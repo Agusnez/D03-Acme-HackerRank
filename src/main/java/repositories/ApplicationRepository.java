437e93af76fae0da2815276804f669e17814a54f
Souto <jasvbetico@hotmail.com>
2019-04-10 19:06:38 +0200

    Update ApplicationRepository.java

--- src/main/java/repositories/ApplicationRepository.java
+++ src/main/java/repositories/ApplicationRepository.java
@@ -12,28 +12,34 @@ import domain.Application;
 @Repository
 public interface ApplicationRepository extends JpaRepository<Application, Integer> {
 
-	@Query("select a from Application a where a.hacker.id = ?1 and a.status = 'ACCEPTED'")
+	@Query("select a from Application a where a.hacker.id = ?1 and a.status = 'ACCEPTED' and a.position.deadline > CURRENT_DATE")
 	Collection<Application> findAllAcceptedByHacker(int hackerId);
 
-	@Query("select a from Application a where a.hacker.id = ?1 and a.status = 'REJECTED'")
+	@Query("select a from Application a where a.hacker.id = ?1 and a.status = 'REJECTED' and a.position.deadline > CURRENT_DATE")
 	Collection<Application> findAllRejectedByHacker(int hackerId);
 
-	@Query("select a from Application a where a.hacker.id = ?1 and a.status = 'PENDING'")
+	@Query("select a from Application a where a.hacker.id = ?1 and a.status = 'PENDING' and a.position.deadline > CURRENT_DATE")
 	Collection<Application> findAllPendingByHacker(int hackerId);
 
-	@Query("select a from Application a where a.hacker.id = ?1 and a.status = 'SUBMITTED'")
+	@Query("select a from Application a where a.hacker.id = ?1 and a.status = 'SUBMITTED' and a.position.deadline > CURRENT_DATE")
 	Collection<Application> findAllSubmittedByHacker(int hackerId);
 
-	@Query("select a from Application a where a.position.company.id = ?1 and a.status = 'ACCEPTED'")
+	@Query("select a from Application a where a.hacker.id = ?1 and a.position.deadline < CURRENT_DATE")
+	Collection<Application> findAllDeadLinePastByHacker(int hackerId);
+
+	@Query("select a from Application a where a.position.company.id = ?1 and a.status = 'ACCEPTED' and a.position.deadline > CURRENT_DATE")
 	Collection<Application> findAllAcceptedByCompany(int companyId);
 
-	@Query("select a from Application a where a.position.company.id = ?1 and a.status = 'REJECTED'")
+	@Query("select a from Application a where a.position.company.id = ?1 and a.status = 'REJECTED' and a.position.deadline > CURRENT_DATE")
 	Collection<Application> findAllRejectedByCompany(int companyId);
 
-	@Query("select a from Application a where a.position.company.id = ?1 and a.status = 'PENDING'")
+	@Query("select a from Application a where a.position.company.id = ?1 and a.status = 'PENDING' and a.position.deadline > CURRENT_DATE")
 	Collection<Application> findAllPendingByCompany(int companyId);
 
-	@Query("select a from Application a where a.position.company.id = ?1 and a.status = 'SUBMITTED'")
+	@Query("select a from Application a where a.position.company.id = ?1 and a.status = 'SUBMITTED' and a.position.deadline > CURRENT_DATE")
 	Collection<Application> findAllSubmittedByCompany(int companyId);
+
+	@Query("select a from Application a where a.hacker.id = ?1 and a.position.deadline < CURRENT_DATE")
+	Collection<Application> findAllDeadLinePastByCompany(int companyId);
 
 }
