15b5272f046b56eff238d6743b8a20148625b9bb
Raul Navarro Cruz <esthalo17@gmail.com>
2019-04-12 19:50:37 +0200

    test edit curticulum y datas y populate

--- src/test/java/services/MiscellaneousDataServiceTest.java
+++ src/test/java/services/MiscellaneousDataServiceTest.java
@@ -76,4 +76,48 @@ public class MiscellaneousDataServiceTest extends AbstractTest {
 		super.checkExceptions(expected, caught);
 
 	}
+
+	@Test
+	public void driverEditMiscellanousData() {
+		final Object testingData[][] = {
+			{
+				"miscellaneousData1", "test", null
+			},//1. All fine
+			{
+				"miscellaneousData1", "		", ConstraintViolationException.class
+			},//1. All fine
+			{
+				"miscellaneousData1", null, ConstraintViolationException.class
+			},//1. All fine
+		};
+
+		for (int i = 0; i < testingData.length; i++)
+			this.templateEditMiscellanousData((String) testingData[i][0], (String) testingData[i][1], (Class<?>) testingData[i][2]);
+	}
+	protected void templateEditMiscellanousData(final String dataBean, final String text, final Class<?> expected) {
+
+		Class<?> caught;
+
+		caught = null;
+		try {
+
+			this.startTransaction();
+
+			final MiscellaneousData data = this.miscellaneousDataService.findOne(super.getEntityId(dataBean));
+			data.setText(text);
+
+			this.miscellaneousDataService.save(data);
+			this.miscellaneousDataService.flush();
+
+		} catch (final Throwable oops) {
+			caught = oops.getClass();
+		}
+
+		this.unauthenticate();
+
+		this.rollbackTransaction();
+
+		super.checkExceptions(expected, caught);
+
+	}
 }
